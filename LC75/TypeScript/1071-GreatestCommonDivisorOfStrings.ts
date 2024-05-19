2b125b8cb91c60cbf5157ec1d465b5af7d0c52af
Martin-Enghoy <adrianenghoy@gmail.com>
2024-05-20 00:25:08 +0800

    ace75: ts array q2

---
+++ LC75/TypeScript/1071-GreatestCommonDivisorOfStrings.ts
@@ -0,0 +1,14 @@
+function gcdOfStrings(str1: string, str2: string): string {
+  // If the concatenated strings are not equal,
+  // then a common divisor cannot be found
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  }
+
+  // Calculate the greatest common divisor of the string lengths
+  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+  const len = gcd(str1.length, str2.length);
+
+  // Return the substring that is a common divisor
+  return str1.substring(0, len);
+}
