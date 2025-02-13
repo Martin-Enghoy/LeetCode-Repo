d9e3f1691076200c2c10ad27cbd21f5e37e6c5be
Martin-Enghoy <adrianenghoy@gmail.com>
2025-02-13 10:00:23 +0800

    lc75: 643 final

--- LC75/TypeScript/sliding-window/643-MaximumAverageSubarray1.ts
+++ LC75/TypeScript/sliding-window/643-MaximumAverageSubarray1.ts
@@ -1,3 +1,11 @@
+/*
+* You are given an integer array nums consisting of n elements, and an integer k.
+*
+* Find a contiguous subarray whose length is equal to k
+* that has the maximum average value and return this value.
+* Any answer with a calculation error less than 10-5 will be accepted.
+*
+* */
 function findMaxAverage(nums: number[], k: number): number {
     let left = 0, right = k-1, ans = 0, acc = 0;
 
@@ -17,6 +25,34 @@ function findMaxAverage(nums: number[], k: number): number {
         left++;
         right++;
         acc = 0;
+    }
+
+    return ans;
+}
+
+function findMaxAverageFinal(nums: number[], k: number): number {
+    let left = 0, right = k-1, ans = 0, acc = 0;
+
+    if (nums.length < k) {
+        return 0;
+    }
+
+    for (let i = left; i <= right; i++) {
+        acc += nums[i];
+    }
+
+    ans = acc / k;
+
+    while (right < nums.length) {
+        acc -= nums[left];
+        acc += nums[right + 1];
+
+        if ((acc / k) > ans) {
+            ans = acc / k;
+        }
+
+        left++;
+        right++;
     }
 
     return ans;
