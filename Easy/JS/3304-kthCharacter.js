ab0ea9672645553c5f1198048ed1e9e3f2a92c36
Martin-Enghoy <adrianenghoy@gmail.com>
2025-07-03 09:20:38 +0800

    feat: 3304 js

---
+++ Easy/JS/3304-kthCharacter.js
@@ -0,0 +1,37 @@
+function toInt(char) {
+  // Convert the character to lowercase to handle both 'A' and 'a'
+  const lowerChar = char.toLowerCase();
+
+  // Get the Unicode (ASCII) value of 'a'
+  const asciiOfA = 'a'.charCodeAt(0);
+
+  // Get the Unicode (ASCII) value of the input character
+  const asciiOfChar = lowerChar.charCodeAt(0);
+
+  // Calculate the integer equivalent (e.g., 'a' - 'a' + 1 = 1)
+  return asciiOfChar - asciiOfA + 1;;
+}
+
+function kthCharacter(k) {
+  let str = ['a'];
+
+  while (str.length < k) {
+    const next = [];
+
+    for (const char of str) {
+      const curr = toInt(char);
+      const first = toInt('a');
+
+      const calcStr = String.fromCharCode(65 + ((curr - first + 1) % 26)).toLowerCase();
+
+      next.push(calcStr);
+    }
+
+    str = [
+      ...str,
+      ...next,
+    ];
+  }
+
+  return str[k - 1];
+}
